3e63cbce96b290d8180c779f67b0f07b8c4f9e1c
Silvio Paganini <s2paganini@gmail.com>
2015-06-19 14:47:48 +0100

    add more options

--- ribbon/demo.js
+++ ribbon/demo.js
@@ -14,7 +14,9 @@ var clock = new THREE.Clock();
 var particles = new Array(200);
 var PARAMS = window.PARAMS = {
     speed : .4,
-    randomY : true
+    randomY : true,
+    particles : 200,
+    interval : 25
 }
 
 function init()
@@ -35,15 +37,17 @@ function init()
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 4000 );
     camera.position.set(12, 17, 50);
 
+    onResize();
+
     controls = new OrbitControls(camera, renderer.domElement);
-    window.c = camera;
     controls.maxDistance = 5000;
     // controls.minDistance = 50;
 
     scene = new THREE.Scene();
     scene.add(new THREE.AmbientLight(0xfcfcfc));
 
-    draw();
+    reset();
+    update();
 }
 
 function draw()
@@ -54,8 +58,21 @@ function draw()
 
         setTimeout(function(p){
             p.init();
-        }, 25 * i, particles[i]);
+        }, window.PARAMS.interval * i, particles[i]);
     };
+}
+
+function reset()
+{
+    for (var i = 0; i < particles.length; i++) {
+        if(particles[i]) {
+            scene.remove(particles[i].mesh);
+            particles[i] = null;
+        }
+    };
+
+    particles = new Array(window.PARAMS.particles >> 0);
+    draw();
 }
 
 function update()
@@ -64,7 +81,7 @@ function update()
     dt = clock.getDelta();
 
     for (var i = particles.length - 1; i >= 0; i--) {
-        particles[i].update(dt);
+        if(particles[i]) particles[i].update(dt);
     };
 
     renderer.render(scene, camera);
@@ -72,14 +89,15 @@ function update()
     requestAnimationFrame(update);
 }
 
+
 init();
 
 var gui = new dat.GUI()
 gui.add(window.PARAMS, 'speed', .2, .8);
+gui.add(window.PARAMS, 'particles', 10, 300).onChange(reset.bind(this));
+gui.add(window.PARAMS, 'interval', 0, 400).name('particle step').onChange(reset.bind(this));
 gui.add(window.PARAMS, 'randomY').name('random Y start')
 
-onResize();
-update();
 window.onresize = onResize;
 function onResize(){
     videoLayer = document.getElementsByTagName('video')[0]
